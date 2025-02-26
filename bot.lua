a85469c3a8a632ed3eea67548c4e99c3f0db149b
uglev <46020541+uglev@users.noreply.github.com>
2025-02-26 11:15:07 +0300

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -34,22 +34,28 @@ function api.on_message(message)
     end
 
     local text = message.text:lower()
-    local buttons = {}
-    local search_text = text
+    local words = {}
+    for word in text:gmatch("%S+") do
+      table.insert(words, word)
+    end
 
-    while #search_text > 3 do
-      for _, result in ipairs(results) do
-        if #buttons >= 3 then
+    local buttons = {}
+    for _, word in ipairs(words) do
+      local search_text = word
+      while #search_text > 3 do
+        for _, result in ipairs(results) do
+          if #buttons >= 3 then
+            break
+          end
+          if result.name:lower():find(search_text, 1, true) then
+            table.insert(buttons, {text = result.name, callback_data = result.name})
+          end
+        end
+        if #buttons > 0 then
           break
         end
-        if result.name:lower():find(search_text, 1, true) then
-          table.insert(buttons, {text = result.name, callback_data = result.name})
-        end
+        search_text = utf8_sub(search_text, 1, utf8.len(search_text) - 1)
       end
-      if #buttons > 0 then
-        break
-      end
-      search_text = utf8_sub(search_text, 1, utf8.len(search_text) - 1)
     end
 
     os.execute("sleep 0.5")
